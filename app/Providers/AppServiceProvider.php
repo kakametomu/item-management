e8bdcce4b62cd2a259001c4c7f75b434bc4eb840
kakametomu <111382108+kakametomu@user.noreply.github.com>
2023-02-04 21:27:46 +0900

    https化

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Schema;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -25,10 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
-        
         if (\App::environment(['production'])) {
             \URL::forceScheme('https');
         }
+
+        Schema::defaultStringLength(191);
     }
 }
